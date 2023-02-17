9b34e762fe4929b3c2f5dfd6693e70be50ae028b
Sonechka 3 <106232746+Sofffiaaa@users.noreply.github.com>
2023-02-17 13:29:17 +0300

    ///

--- Test/AppDelegate.swift
+++ Test/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let encoder = JSONEncoder()
     private let assembly = Assembly()
     
+    let profile = "profile"
+    
     lazy var dataStorage = assembly.dataStorage
     lazy var apiClient = assembly.apiClient
     
@@ -24,10 +26,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         apiClient.profile(_type: ProfileResponseData.self){ result in
             switch result {
             case .success(let profile):
+                guard let profile = try? result.get().data else {
+                    return
+                }
+                self.dataStorage.save(value: profile.profile, key: "key")
                 print("Download successful \(profile)")
+                self.dataStorage.save(value: profile, key: "key")
             case .failure(let error):
                 print("Download error \(error)")
             }
+        }
+        
+        if let profile: Profile = dataStorage.value(key: "key") {
+            print(profile)
         }
         
         print("...end load profile \(Thread.isMainThread)")
